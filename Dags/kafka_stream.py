5eee7758828579dbf45942091bddcf651cfa367a
samyak-anand <samyak.annad@gmail.com>
2024-04-14 20:28:28 +0200

    inital Commit

--- Dags/kafka_stream.py
+++ Dags/kafka_stream.py
@@ -14,7 +14,31 @@ def get_data():
     res = requests.get("https://randomuser.me/api/")
     res = res.json()
     res = res['results'][0]
-    print(res)
+    return res
+    print(json.dumps(res,indent=3))
 
+def format_data(res):
+    data = {}
+    location = res['location']
+    #data['id'] = uuid.uuid4()
+    data['first_name'] = res['name']['first']
+    data['last_name'] = res['name']['last']
+    data['gender'] = res['gender']
+    data['address'] = f"{str(location['street']['number'])} {location['street']['name']}, " \
+                      f"{location['city']}, {location['state']}, {location['country']}"
+    data['post_code'] = location['postcode']
+    data['email'] = res['email']
+    data['username'] = res['login']['username']
+    data['dob'] = res['dob']['date']
+    data['registered_date'] = res['registered']['date']
+    data['phone'] = res['phone']
+    data['picture'] = res['picture']['medium']
 
-get_data()
+    return data
+def stream_data():
+    import json
+    res= get_data()
+    res= format_data(res)
+    print(json.dumps(res,indent=3))
+
+stream_data()
